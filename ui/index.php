0cc502609a8addf3a8dc73dcb029ef96bdfdc50d
Mohit Singh <ask4mohitdrocker@gmail.com>
2018-02-21 10:55:07 +0530

    Update index.php

--- ui/index.php
+++ ui/index.php
@@ -1,16 +1,20 @@
-<?php
-    <head>
-        <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-        <div class="center">
-            <img src="/ui/madi.png" class="img-medium"/>
-        </div>
-        <br>
-        <div class="center text-big bold">
-            echo Hi! I am Mohit, and this is my first webapp.
-        </div>
-        <script type="text/javascript" src="/ui/main.js">
-        </script>
-    </body>
+<? php
+echo '<!doctype html>';
+echo '<html>';
+echo '<head>';
+echo '<link href="/ui/style.css" rel="stylesheet" />';
+echo '</head>';
+echo '<body>';
+echo '<div class="center">';
+echo '<img src="/ui/madi.png" class="img-medium"/>';
+echo '</div>';
+echo '<br>';
+echo '<div class="center text-big bold">';
+echo 'Hi! I am Mohit, and this is my first webapp.';
+echo '</div>';
+echo '<script type="text/javascript" src="/ui/main.js">';
+echo '</script>';
+echo '</body>';
+echo '</html>';
+echo '';
 ?>
